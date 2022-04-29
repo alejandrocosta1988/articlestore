45e69d1b626c83944fc7fd2685a7c4b45de8c16c
Alejandro Costa <alejandroesfc@gmail.com>
2022-04-29 07:22:40 -0300

    creates a User as BeforeEach method

--- test/model/UserTest.java
+++ test/model/UserTest.java
@@ -1,17 +1,24 @@
 package model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class UserTest {
 
+	private User user;
 	private String firstName;
+	
+	@BeforeEach
+	void createUser() {
+		user = new User();
+	}
 
 	@Test
 	void isValidReturnsFalseWhenSomeAttributeIsNotProvided() {
 		
-		User user = new User();
 		user.setEmail("teste@email.com");
 		user.setLastName("Costa");
 		
@@ -22,7 +29,6 @@ class UserTest {
 	@Test
 	void isValidReturnsTrueWhenAllAttributesAreProvided() {
 		
-		User user = new User();
 		user.setFirstName("Teste");
 		user.setLastName("Testado");
 		user.setEmail("teste@email.com");
@@ -34,7 +40,7 @@ class UserTest {
 	@Test
 	void isValidReturnsFalseWhenSomettributeIsDefinedAsNull() {
 		
-		User user = new User(firstName, "Teste", "teste@email.com");
+		user = new User(firstName, "Teste", "teste@email.com");
 		
 		assertFalse(user.isValid());
 		
